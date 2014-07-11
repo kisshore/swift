439f373843ac22a4dcf419c95f202cca041f653c
kisshore <kishorerbjv@gmail.com>
2014-07-11 09:48:37 +0000

    Added styling

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,23 +1,41 @@
 class WelcomeController < ApplicationController
+  require "json"
+  require 'net/ping'
+  
   def index
   end
   
   def upload
+    
+    generate_token
+      if(@a_token_id)
+        flash[:id_notice]= "New authentication token has benn generated.!"
+    else
+      flash[:id_notice]= "Looks like there is an error.. unable to create authentication token, sorry.!"
+    end
+    
   end
   
   def upload_swift
     p "$"*100
     puts params 
-    
-   p "**"*100
-    k = %x(ping -c 2 192.168.5.75)
-    if(k.length > 0)
-    @ex = %x(  curl -H "Content-Type: application/json" -d '{"auth":{"passwordCredentials":{"username":"swift","password":"root"},"tenantName":"service" }}' http://192.168.5.75:5000/v2.0/tokens )
-    
-    p @ex
-   p "**"*100 
-    end
     redirect_to :back
     
   end
+  
+  private
+  
+  def generate_token
+    p "**"*100
+      if(Net::Ping::External.new("192.168.5.75").ping?)
+        p "192.168.5.75  is alive!"
+        a_token = %x(  curl -H "Content-Type: application/json" -d '{"auth":{"passwordCredentials":{"username":"swift","password":"root"},"tenantName":"service" }}' http://192.168.5.75:5000/v2.0/tokens )
+        @a_token = JSON.parse(a_token) 
+        @a_token_id = @a_token["access"]["token"]["id"]
+      end
+   p "**"*100 
+   
+  end
+  
+  
 end
